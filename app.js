6f3d3d1c6cbc10a5b844009133aed043b048a86a
aahan_5833 <aahancharak@gmail.com>
2019-09-26 14:44:00 +0530

    Completed the basic functioning,few more changes to be added

--- app.js
+++ app.js
@@ -1,52 +1,66 @@
-const calculateModule = (function () {
-
+const calculateModule = (function() {
   return {
-    calculateTip: function (bill, tip, number) {
-      return (bill + (tip * bill / 100) / number).toFixed(5);
-
+    calculateTip: function(bill, tip, number) {
+      bill = bill;
+      tip = tip;
+      number = number;
+      let tipCalc = (bill + (tip * bill) / 100) / number;
+      return tipCalc.toFixed(2);
     }
-  }
-
-
-
-
+  };
 })();
 
-
-const uiModule = (function () {
+const uiModule = (function() {
   let DomStrings = {
     amount: 'bill__amount',
     per: 'tip__percentage',
-    num: 'customer__number'
-  }
+    num: 'customer__number',
+    billArea: '.display__text'
+  };
   return {
-    getDomStrings: function () {
+    getDomStrings: function() {
       return DomStrings;
+    },
+    displayData: function(eachBill) {
+      document.querySelector(
+        DomStrings.billArea
+      ).innerHTML = `Bill to be shared by each member is $${eachBill}`;
     }
-  }
-
-
-
+  };
 })();
 
-
-const controllerModule = (function (calMod, uiMod) {
+const controllerModule = (function(calMod, uiMod) {
   //To get the input from the user
   let dom = uiMod.getDomStrings();
   //Setting up the button click event listener
 
-  const getData = function () {
-    var bill = document.getElementById(dom.amount).value;
-    var tip = document.getElementById(dom.per).value;
-    var number = document.getElementById(dom.num).value;
-
-  }
+  const getData = function() {
+    var bill = parseInt(document.getElementById(dom.amount).value, 10);
+    var tip = parseInt(document.getElementById(dom.per).value, 10);
+    var number = parseInt(document.getElementById(dom.num).value, 10);
+    if (
+      bill < 5 ||
+      number < 1 ||
+      tip === undefined ||
+      number === undefined ||
+      bill === undefined ||
+      number > 30
+    ) {
+      if (number > 30) {
+        alert(
+          `Sorry but we can't have a party of more than 30 in our restaurant`
+        );
+      } else {
+        alert('Invalid input');
+      }
+    } else {
+      var eachBill = calMod.calculateTip(bill, tip, number);
+      //To display the data
+      uiMod.displayData(eachBill);
+    }
+  };
 
   document.querySelector('.submit__button').addEventListener('click', getData);
 
   //Calculating the bill function
-
-
-
-
 })(calculateModule, uiModule);
